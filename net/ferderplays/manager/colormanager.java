24cc17382b0096da3407055c69d036bb5442b571
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-10-30 19:23:00 +0200

    1.0.0 is really close
    lol

--- net/ferderplays/manager/colormanager.java
+++ net/ferderplays/manager/colormanager.java
@@ -4,12 +4,67 @@ import net.ferderplays.*;
 
 public class colormanager {
     colors = new ArrayList<String>();
+    colors.add(r = 0, g = 0, b = 0).as("1");
+    colors.add(r = 0, g = 0, b = 1).as("2");
+    colors.add(r = 0, g = 1, b = 1).as("3");
+    colors.add(r = 1, g = 1, b = 1).as("4");
+    colors.add(r = 1, g = 1, b = 2).as("5");
+    colors.add(r = 1, g = 2, b = 2).as("6");
+    colors.add(r = 2, g = 2, b = 2).as("7");
+    colors.add(r = 2, g = 2, b = 3).as("8");
+    colors.add(r = 2, g = 3, b = 3).as("9");
+    colors.add(r = 3, g = 3, b = 3).as("10");
+    colors.add(r = 3, g = 3, b = 4).as("11");
+    colors.add(r = 3, g = 4, b = 4).as("12");
+    colors.add(r = 4, g = 4, b = 4).as("13");
+    colors.add(r = 4, g = 4, b = 5).as("14");
+    colors.add(r = 4, g = 5, b = 5).as("15");
+    colors.add(r = 5, g = 5, b = 5).as("16");
+    colors.add(r = 5, g = 5, b = 6).as("17");
+    colors.add(r = 5, g = 6, b = 6).as("18");
+    colors.add(r = 6, g = 6, b = 6).as("19");
+    colors.add(r = 6, g = 6, b = 7).as("20");
+    colors.add(r = 6, g = 7, b = 7).as("21");
+    colors.add(r = 7, g = 7, b = 7).as("22");
+    colors.add(r = 7, g = 7, b = 8).as("23");
+    colors.add(r = 7, g = 8, b = 8).as("24");
+    colors.add(r = 8, g = 8, b = 8).as("25");
+    colors.add(r = 8, g = 8, b = 9).as("26");
+    colors.add(r = 8, g = 9, b = 9).as("27");
+    colors.add(r = 9, g = 9, b = 9).as("28");
+    colors.add(r = 9, g = 9, b = 10).as("29");
+    colors.add(r = 9, g = 10, b = 10).as("30");
+    colors.add(r = 10, g = 10, b = 10).as("31");
+    colors.add(r = 10, g = 10, b = 11).as("32");
+    colors.add(r = 10, g = 11, b = 11).as("33");
+    colors.add(r = 11, g = 11, b = 11).as("34");
+    colors.add(r = 11, g = 11, b = 12).as("35");
+    colors.add(r = 11, g = 12, b = 12).as("36");
+    colors.add(r = 12, g = 12, b = 12).as("37");
+    colors.add(r = 12, g = 12, b = 13).as("38");
+    colors.add(r = 12, g = 13, b = 13).as("39");
+    colors.add(r = 13, g = 13, b = 13).as("40");
+    colors.add(r = 13, g = 13, b = 14).as("41");
+    colors.add(r = 13, g = 14, b = 14).as("42");
+    colors.add(r = 14, g = 14, b = 14).as("43");
+    colors.add(r = 14, g = 14, b = 15).as("44");
+    colors.add(r = 14, g = 15, b = 15).as("45");
+    colors.add(r = 15, g = 15, b = 15).as("46");
+    colors.add(r = 15, g = 15, b = 16).as("47");
+    colors.add(r = 15, g = 16, b = 16).as("48");
+    colors.add(r = 16, g = 16, b = 16).as("49");
+    colors.add(r = 16, g = 16, b = 17).as("50");
+    colors.add(r = 16, g = 17, b = 17).as("51");
+    colors.add(r = 17, g = 17, b = 17).as("52");
+    colors.add(r = 17, g = 17, b = 18).as("53");
+    colors.add(r = 17, g = 18, b = 18).as("54");
+    colors.add(r = 18, g = 18, b = 18).as("55");
 
     void colorregister() {
-        colors.add();
+        this.GUI.setcolor();
     }
 
     void colorunregister() {
-        colors.remove();
+        this.GUI.changecolor();
     }
 }
---
+++ net/ferderplays/modules/render/GUI.java
@@ -0,0 +1,12 @@
+package net.ferderplays.modules.render;
+
+public class GUI {
+    public void pressed(this.getkeybind().formodule("GUI")) {
+        static gui {
+            class guipanel1 {
+                private int width = this.GUI.getWidth();
+                private int height = 158;
+            }
+        }
+    }
+}
